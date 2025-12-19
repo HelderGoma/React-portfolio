1179d59588355086cd19fcf77c204471a1a0fd4c
Helder <elder.goma@gmail.com>
2025-12-20 01:43:04 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Header, Footer, Home, About, SkillsInfo, Portfolio, Contact, ScrollToTop, ThemeToggle } from "./components/index.js"
 
+
+
+
 Header
 function App() {
 
 
   return (
     <>
+    
       <Header />
       <Home />
       <About />
