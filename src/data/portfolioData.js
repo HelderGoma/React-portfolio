c8b71661e99ccc32fba40636337a3e9028ea376f
Helder <elder.goma@gmail.com>
2026-02-09 03:53:47 +0300

    Update portfolioData.js

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,5 +1,5 @@
 
-import { portfolio1, portfolio2, portfolio3, portfolio4, portfolio5} from "../assets/images"
+import { portfolio1, portfolio2, portfolio3, portfolio4, portfolio5 } from "../assets/images"
 
 export const portfolioData = [
     {
@@ -32,14 +32,14 @@ export const portfolioData = [
         title: "E-Commerce Product Catalog (React + Redux Toolkit)",
         category: "Web Application / TypeScript",
         imgSrc: portfolio4,
-        description: "Product catalog built with React and TypeScript.The project includes product listing, product cards, and a fully functional shopping cart with quantity management and persistence via localStorage.State management is implemented using Redux Toolkit.The application follows a modular architecture and includes unit and component tests using Vitest and React Testing Library. https://reacttscatalog.netlify.app/"
+        description: "Product catalog built with React and TypeScript.The project includes product listing, product cards, and a fully functional shopping cart with quantity management and persistence via localStorage.State management is implemented using Redux Toolkit.The application follows a modular architecture and includes unit and component tests using Vitest and React Testing Library. https://heldergoma.github.io/React-catalog/#/"
     },
     {
         id: 5,
         title: "Practice Hub: Frontend Lab",
         category: "Web UI Experiments",
         imgSrc: portfolio5,
-        description: "A collection of practice mini projects featuring responsive layouts, interactive UI elements, and basic web functionality to showcase frontend development skills. https://frontendlabhgoma.netlify.app/"
+        description: "A collection of practice mini projects featuring responsive layouts, interactive UI elements, and basic web functionality to showcase frontend development skills. https://heldergoma.github.io/Pet-Projects/#/"
     }
 
 ]
